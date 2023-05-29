2e47606ffb57d3e00979990db64847a08055c368
Cecilie Bossov Westerdahl <eaacbw@students.eaaa.dk>
2023-05-29 18:19:08 +0200

    Der manglede lige noget

--- AuthService/Controllers/AuthController.cs
+++ AuthService/Controllers/AuthController.cs
@@ -1,19 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Text;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 using AuthService.Services;
 using AuthService.Models;
-using VaultSharp;
-using VaultSharp.V1.AuthMethods.Token;
-using VaultSharp.V1.AuthMethods;
-using VaultSharp.V1.Commons;
 
 namespace AuthService.Controllers
 {
@@ -22,37 +10,32 @@ namespace AuthService.Controllers
     [Route("authservice/v1")]
     public class AuthController : ControllerBase
     {
+        // Attributter
         private readonly ILogger<AuthController> _logger;
-
         private readonly IConfiguration _config;
         private readonly ILoginService _loginService;
-       
-  
+
         // Konstruktør, der tager en logger og en konfiguration som argumenter:
         public AuthController(ILogger<AuthController> logger, IConfiguration config, ILoginService loginService)
         {
             _logger = logger;
-            _config = config; 
+            _config = config;
             _loginService = loginService;
         }
 
-        // Angiver, at denne metode kan tilgås uden autentifikation:
-        [AllowAnonymous]
-        // Håndterer HTTP POST-anmodninger til "/authservice/v1/login" og tager imod en LoginInfo som parameter:
-        [HttpPost("login")]
+        [AllowAnonymous] // Angiver, at denne metode kan tilgås uden autentifikation
+        [HttpPost("login")] // Håndterer HTTP POST-anmodninger til "/authservice/v1/login" og tager imod en LoginInfo som parameter
         public async Task<IActionResult> Login([FromBody] LoginInfo login)
         {
             try
             {
-                _logger.LogInformation("Login metode ramt, dette er dine login oplysinger:" + login.Email + login.AccessCode);
-                // Kalder Login-metoden på LoginService og returnerer resultatet:   
-                return await _loginService.Login(login);
+                _logger.LogInformation("Login metode ramt, dette er dine login oplysinger:" + login.Email + login.AccessCode);  
+                return await _loginService.Login(login); // Kalder Login-metoden på LoginService og returnerer resultatet
             }
             catch (Exception ex)
             {
-                // Hvis der opstår en fejl, logges den og kastes som en undtagelse:
-                _loginService._logger.LogError($"Fejl ved login metode: {ex.Message}");
-                throw;
+                _loginService._logger.LogError($"Fejl ved login metode: {ex.Message}"); // Hvis der opstår en fejl, logges den
+                throw; // Kastes som en undtagelse
             }
         }
 
@@ -60,45 +43,38 @@ namespace AuthService.Controllers
         [HttpGet("gateway")]
         public async Task<IActionResult> NginxAuth()
         {
-        return Ok("You're authorized");
+            return Ok("You're authorized");
         }
-       
+
         [AllowAnonymous]
-        // Håndterer HTTP POST-anmodninger til "/authservice/v1/validate" og tager imod en token-streng som parameter:
-        [HttpPost("validate")]
+        [HttpPost("validate")] // Håndterer HTTP POST-anmodninger til "/authservice/v1/validate" og tager imod en token-streng som parameter
         public async Task<IActionResult> ValidateJwtToken([FromBody] string? token)
         {
             try
             {
-                // Kalder ValidateJwtToken-metoden på LoginService og returnerer resultatet:
-                return await _loginService.ValidateJwtToken(token);
+                return await _loginService.ValidateJwtToken(token); // Kalder ValidateJwtToken-metoden på LoginService og returnerer resultatet:
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
-               // Hvis der opstår en fejl, logges den og returneres en HTTP-fejlstatus:
-                _logger.LogError(ex, ex.Message);
-                return StatusCode(404);
+                _logger.LogError(ex, ex.Message); // Hvis der opstår en fejl, logges den
+                return StatusCode(404); // Returneres en HTTP-fejlstatus
             }
         }
 
-       
         [HttpGet("version")]
         public IEnumerable<string> Get()
         {
             var properties = new List<string>();
-            var assembly = typeof(Program).Assembly; 
+            var assembly = typeof(Program).Assembly;
 
-            // Henter metadata om assembly'en og tilføjer dem til en liste:
-            foreach (var attribute in assembly.GetCustomAttributesData()) 
+            foreach (var attribute in assembly.GetCustomAttributesData()) // Henter metadata om assembly'en og tilføjer dem til en liste
             {
-                properties.Add($"{attribute.AttributeType.Name} - {attribute.ToString()}"); 
+                properties.Add($"{attribute.AttributeType.Name} - {attribute.ToString()}");
             }
 
-            // Logger information om assembly-metadataen:
-            _logger.LogInformation($"Hentet assembly-metadata for version: {properties}");
-            
-            // Returnerer listen med assembly-metadata:
-            return properties; 
+            _logger.LogInformation($"Hentet assembly-metadata for version: {properties}"); // Logger information om assembly-metadataen
+
+            return properties; // Returnerer listen med assembly-metadata
         }
     }
 }
